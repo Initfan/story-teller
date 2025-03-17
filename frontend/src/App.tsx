4c5aa634977e53681e975f10ae43e746ee98cce2
irfan rifani <feeinitfan@gmail.com>
2025-03-17 22:25:14 +0700

    user dapat memilih kelanjutan cerita

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,10 @@
 import { useState } from "react";
 import StoryCreate from "./story/StoryCreate";
-import StoryView from "./story/StoryView";
 import { storyType } from "./utils/definition";
+import StoryView from "./story/StoryView";
 
 const App = () => {
 	const [story, setStory] = useState<storyType>();
-
-	console.log(story);
 
 	const createStory = async (formData: FormData) => {
 		const req = await fetch("http://localhost:8000/story/generate", {
