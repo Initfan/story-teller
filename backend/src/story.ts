6b39985bd9b416530aa248c79e57be2e2ee292f1
irfan rifani <feeinitfan@gmail.com>
2025-03-17 22:16:34 +0700

    menginstall zod validation dan membuat api continue story

--- backend/src/story.ts
+++ backend/src/story.ts
@@ -1,29 +1,50 @@
 import { Hono } from "hono";
 import { generationConfig, model } from "./utils/gemini.js";
+import { z } from "zod";
 
 const app = new Hono();
+
+const chatSession = model.startChat({
+	generationConfig,
+});
 
 app.post("/generate", async (c) => {
 	const req = await c.req.json();
 
 	if (!req.genre) return c.json("No genre provided");
 
-	const chatSession = model.startChat({
-		generationConfig,
-	});
+	const prompt = `buatkan cerita beserta judulnya yang bergenre ${req.genre}. berikan saya beberapa pilihan untuk melanjutkan kelanjutan ceritanya`;
 
-	let prompt = `buatkan cerita beserta judulnya yang bergenre ${req.genre} `;
-	if (req.option) {
-		prompt += `.berikan saya beberapa pilihan untuk melanjutkan kelanjutan ceritanya`;
-	}
-
-	let result = await chatSession.sendMessage(prompt);
+	const result = await chatSession.sendMessage(prompt);
 
 	return c.json({
 		success: true,
 		data: result.response.text(),
 		messsage: "Story created",
 	});
+});
+
+app.post("/continue", async (c) => {
+	const req = await c.req.json();
+
+	const validation = z
+		.object({
+			option: z.string(),
+		})
+		.safeParse(req);
+
+	if (!validation.success) return c.json(validation.error);
+
+	try {
+		const newStory = await chatSession.sendMessage(validation.data.option);
+		return c.json({
+			success: true,
+			data: newStory.response.text(),
+			message: "Story created",
+		});
+	} catch (error) {
+		return c.json({ success: false, error });
+	}
 });
 
 export default app;
