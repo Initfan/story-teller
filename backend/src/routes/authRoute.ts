512cd0153531a3f9d97bd456e10e0fb7b675dc63
irfan rifani <feeinitfan@gmail.com>
2025-03-27 20:27:03 +0700

    api login

---
+++ backend/src/routes/authRoute.ts
@@ -0,0 +1,65 @@
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client";
+import bcrypt from "bcrypt";
+import { z } from "zod";
+import { deleteCookie, getCookie, setCookie } from "hono/cookie";
+
+type Variables = {
+	user: any;
+};
+
+const app = new Hono<{ Variables: Variables }>();
+const prisma = new PrismaClient();
+
+app.post("/login", async (c) => {
+	try {
+		const validation = z
+			.object({
+				email: z.string().email(),
+				password: z.string().min(6),
+			})
+			.safeParse(await c.req.json());
+
+		if (!validation.success)
+			return c.json({ error: validation.error.errors }, 400);
+
+		const { email, password } = validation.data;
+		const user = await prisma.user.findUnique({
+			where: { email },
+		});
+
+		if (user && (await bcrypt.compare(password, user.password))) {
+			const token = crypto.randomUUID().split("-")[0];
+
+			await prisma.user.update({
+				where: { email },
+				data: {
+					token,
+				},
+			});
+
+			c.set("user", user);
+
+			setCookie(c, "token", token, {
+				httpOnly: true,
+				// secure: true,
+				maxAge: 3600,
+				sameSite: "Strict",
+			});
+
+			return c.json({ message: "Login success", token: token });
+		} else {
+			return c.json({ error: "Invalid credentials" }, 401);
+		}
+	} catch (error) {
+		return c.json(
+			{
+				message: "Server error, try again later.",
+				error: error?.toString(),
+			},
+			400
+		);
+	}
+});
+
+export default app;
