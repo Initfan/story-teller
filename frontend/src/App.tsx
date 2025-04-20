5dfe8b597b0cff9d8e574bb6cf3307dd72171cb4
irfan rifani <feeinitfan@gmail.com>
2025-04-20 21:40:41 +0700

    react router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,8 @@
-import { useState } from "react";
-import StoryCreate from "./story/StoryCreate";
-import { storyType } from "./utils/definition";
-import StoryView from "./story/StoryView";
-
 const App = () => {
-	const [story, setStory] = useState<storyType>();
-
-	const createStory = async (formData: FormData) => {
-		const req = await fetch("http://localhost:8000/story/generate", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({
-				genre: formData.getAll("genre"),
-			}),
-		});
-
-		const res = await req.json();
-
-		setStory(res.data);
-	};
-
-	return story ? (
-		<StoryView {...story} />
-	) : (
-		<form action={createStory}>
-			<StoryCreate />
-		</form>
+	return (
+		<>
+			<p>hello world</p>
+		</>
 	);
 };
 
